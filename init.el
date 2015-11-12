2a8fa1280aa371fa96f138f2a1b03419c145f4e0
Dustin Paluch <dustinpaluch@airmail.cc>
2015-11-12 15:11:11 -0600

    Ignore minified files when searching with ag

--- init.el
+++ init.el
@@ -205,6 +205,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-ignore-list (quote ("*.min.*")))
  '(bm-annotate-on-create t)
  '(bm-buffer-persistence t)
  '(bm-cycle-all-buffers t)
