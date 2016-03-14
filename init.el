a2656659e20f0d875bd1ecbb99e91e17742d6ae8
Dustin Paluch <dustinpaluch@airmail.cc>
2016-03-14 11:49:37 -0500

    Change font

--- init.el
+++ init.el
@@ -379,7 +379,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#161A1F" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Menlo"))))
+ '(default ((t (:inherit nil :stipple nil :background "#161A1F" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Roboto Mono"))))
  '(bm-fringe-persistent-face ((t (:background "selectedMenuItemColor" :foreground "White"))))
  '(erc-current-nick-face ((t (:foreground "cyan" :weight bold))))
  '(erc-input-face ((t (:foreground "#777"))))
