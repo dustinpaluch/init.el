725ba0b11f3efbcbf266f149f5145a50979e0b1c
Dustin Paluch <dustinpaluch@airmail.cc>
2016-10-20 09:25:01 -0500

    Bind C-S-s to swiper-all

--- init.el
+++ init.el
@@ -157,6 +157,7 @@ to the current line, then call copy-region-as-kill."
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-S-s") 'swiper-all)
 (global-set-key "\C-r" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key [f6] 'ivy-resume)
