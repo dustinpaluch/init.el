53e538452733bcd9b04368a5f82baaf9d3e44d42
Dustin Paluch <dustinpaluch@airmail.cc>
2019-03-27 11:48:49 -0500

    Add counsel-mark-ring binding

--- init.el
+++ init.el
@@ -159,7 +159,9 @@
 		 ("M-x" . counsel-M-x)
 		 ("C-c g" . counsel-git)
 		 ("C-c j" . counsel-git-grep)
-		 ("C-c k" . counsel-ag))) ; counsel
+		 ("C-c k" . counsel-ag)
+		 ("C-c SPC" . counsel-mark-ring))) ; counsel
+
   :defer t
 
 (use-package cus-edit
