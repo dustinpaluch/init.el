6b6cf88c5fd08a670c6a5cb04eebddf698beb8bb
Dustin Paluch <dustinpaluch@airmail.cc>
2019-03-27 13:19:35 -0500

    Add customize.el content

---
+++ customize.el
@@ -0,0 +1,67 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-modules
+   (quote
+	(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate)))
+ '(erc-server-auto-reconnect nil)
+ '(explicit-shell-file-name "/usr/local/bin/bash")
+ '(hl-paren-colors
+   (quote
+	("red" "orange" "yellow" "green" "cyan3" "DodgerBlue3" "SlateBlue3" "HotPink3")))
+ '(ielm-dynamic-return nil)
+ '(linum-format " %7i ")
+ '(mu4e-headers-skip-duplicates t)
+ '(org-pomodoro-clock-break t)
+ '(package-selected-packages
+   (quote
+	(highlight-indent-guides company geben hydra rjsx-mode a yasnippet-snippets elisp-format ox-reveal dumb-jump json-mode webpaste visual-fill-column markdown-mode seq undo undo- projectile nginx-mode avy smart-mode-line yaml-mode popup vlf color-theme-sanityinc-solarized fireplace counsel ivy swiper zop-to-char yasnippet xcscope which-key wgrep-ag web-mode web-beautify transpose-frame sublime-themes smex smartparens smart-mode-line-powerline-theme shackle rainbow-mode rainbow-delimiters php-mode paredit paradox org-pomodoro multiple-cursors multi-term magit lorem-ipsum linum-relative kv ivy-hydra ibuffer-tramp ibuffer-projectile highlight-parentheses hexrgb git-gutter+ fringe-helper flycheck find-file-in-project f expand-region exec-path-from-shell esxml emmet-mode elisp-slime-nav direx dired+ diff-hl csv-mode company-statistics column-enforce-mode circe buffer-move ag ace-window)))
+ '(paradox-github-token t)
+ '(password-cache nil)
+ '(projectile-completion-system (quote ivy))
+ '(rainbow-html-colors-major-mode-list
+   (quote
+	(html-mode css-mode scss-mode php-mode nxml-mode xml-mode)))
+ '(scss-sass-options (quote ("-E 'UTF-8'")))
+ '(yas-global-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#161A1F" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Roboto Mono"))))
+ '(bm-fringe-persistent-face ((t (:background "selectedMenuItemColor" :foreground "White"))))
+ '(column-enforce-face ((t (:inherit trailing-whitespace))))
+ '(ediff-even-diff-A ((t (:background "#565A5F"))))
+ '(ediff-even-diff-B ((t (:background "#363A3F"))))
+ '(ediff-even-diff-C ((t (:background "#565A5F"))))
+ '(ediff-odd-diff-A ((t (:background "#363A3F"))))
+ '(ediff-odd-diff-B ((t (:background "#565A5F"))))
+ '(ediff-odd-diff-C ((t (:background "#363A3F"))))
+ '(eldoc-highlight-function-argument ((t (:inherit bold :underline t))))
+ '(erc-current-nick-face ((t (:foreground "cyan" :weight bold))))
+ '(erc-input-face ((t (:foreground "#777"))))
+ '(erc-my-nick-face ((t (:foreground "#FF0000" :weight bold))))
+ '(erc-nick-default-face ((t (:foreground "#FD971F" :weight bold))))
+ '(erc-notice-face ((t (:foreground "#2A4B54" :weight bold))))
+ '(erc-prompt-face ((t (:foreground "#EEEEEE" :weight bold))))
+ '(erc-timestamp-face ((t (:foreground "#A6E22E" :weight bold))))
+ '(hl-paren-face ((t (:weight bold))) t)
+ '(ivy-current-match ((t (:background "dark magenta"))))
+ '(ivy-remote ((t (:foreground "coral1"))))
+ '(mu4e-flagged-face ((t (:inherit outline-1 :weight bold))))
+ '(mu4e-header-highlight-face ((t (:inherit swiper-line-face :weight bold))))
+ '(mu4e-replied-face ((t (:inherit outline-4 :weight bold))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "violet"))))
+ '(swiper-line-face ((t (:background "#2A313A"))))
+ '(trailing-whitespace ((t (:background "#21262E"))))
+ '(web-mode-comment-keyword-face ((t (:foreground "Pink" :slant italic :weight bold))))
+ '(web-mode-current-element-highlight-face ((t (:inherit swiper-line-face)))))
